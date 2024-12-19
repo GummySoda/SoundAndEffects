c251c88de56eba20f32831e3b9a7b92b08744db5
Gummy Soda <millsa1@roverkids.org>
2024-12-19 14:17:23 -0500

    3.4 complete

--- SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,26 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool Gameover = false;
-   
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+
+
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
-        
-    
-}
+        playerAudio = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,18 +34,26 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) { isOnGround = true; }
-        else if (collision.gameObject.CompareTag("Obstical"))
+        if (collision.gameObject.CompareTag("Ground"))
+        { 
+            isOnGround = true; 
+            dirtParticle.Play();
+        }else if (collision.gameObject.CompareTag("Obstical"))
         { Gameover = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
         
     }
